a25591a8d395239125463707556b3b1d1171148a
Madan <madan712@gmail.com>
2020-06-06 19:39:57 +0100

    Initial commit

---
+++ src/main/java/com/javaxp/zuulservice/Controller.java
@@ -0,0 +1,21 @@
+package com.javaxp.zuulservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@Value("${spring.application.name}")
+	private String instance;
+
+	@Value("${server.port}")
+	private String port;
+
+	@GetMapping("/")
+	public String message() {
+		return "Hello from " + instance + ":" + port;
+	}
+
+}
